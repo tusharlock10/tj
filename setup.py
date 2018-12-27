f93a4612cc0f1bfcdacf67c1239f5321511f7e91
tusharlock10 <tusharlock10@gmail.com>
2018-12-27 21:00:10 +0530

    Added a setup and LICENSE file.
    Added a setup and LICENSE file.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(  
+    name="tj",
+    version="1.3",
+    description="A very simple short module containing useful functions",
+    long_description="A module that have powerful and simple functions. Read README.txt for more info",
+    author="Tushar Jain",
+    author_email="tusharlock10@gmail.com",
+    py_modules=['tj'],
+    install_requires=['autopy']
+)
